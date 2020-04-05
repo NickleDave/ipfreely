fa88b50d5539b6f8b0f5cff797cd34bba0ab72c7
David Nicholson <NickleDave@users.noreply.github.com>
2020-04-05 17:29:35 -0400

    change constants to be pathlib.Path instances

--- src/ipfreely/constants.py
+++ src/ipfreely/constants.py
@@ -1,3 +1,9 @@
-ADDRESS_FILE = './.ipfreely/current_ip_address.txt'
-SETTINGS_FILE_PATH = './.ipfreely/'
-SETTINGS_FILE = 'settings.txt'
+from pathlib import Path
+
+CONFIG_ROOT = Path('~/.ipfreely').expanduser()
+
+SETTINGS_FILENAME = 'settings.json'
+SETTINGS_PATH = CONFIG_ROOT.joinpath(SETTINGS_FILENAME)
+
+CURRENT_IP_ADDRESS_FILENAME = 'current_ip_address.txt'
+CURRENT_IP_ADDRESS_PATH = CONFIG_ROOT.joinpath(CURRENT_IP_ADDRESS_FILENAME)
